4fd74d16d6399ebde2fd6c247365d5c01d651113
Franco <fnguarachi@gmail.com>
2025-10-04 18:56:52 -0300

    Actualizaciones turnos y home

--- app/src/main/java/com/example/clubdeportivomb/homeActivity.kt
+++ app/src/main/java/com/example/clubdeportivomb/homeActivity.kt
@@ -1,6 +1,9 @@
 package com.example.clubdeportivomb
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,13 @@ class homeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnTurnosMenuActivity = findViewById<Button>(R.id.btnTurnos)
+        btnTurnosMenuActivity.setOnClickListener {
+            val intent = Intent(this, TurnosMenuActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
